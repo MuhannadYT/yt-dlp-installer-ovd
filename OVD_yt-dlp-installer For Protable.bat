c2d794e43cba1fa519e1501f1ca9804cbddd92b5
Muhannad <49660804+MuhannadYT@users.noreply.github.com>
2022-11-10 17:30:38 -0500

    Added the installer for the portable version of OVD

---
+++ OVD_yt-dlp-installer For Protable.bat
@@ -0,0 +1,16 @@
+@ECHO OFF                                                                                                          
+@echo  #   # #####       #####  #      #####           # #    #  ####  #####   ##   #      #      ###### #####  
+@echo   # #    #         #    # #      #    #          # ##   # #        #    #  #  #      #      #      #    # 
+@echo    #     #   ##### #    # #      #    #          # # #  #  ####    #   #    # #      #      #####  #    # 
+@echo    #     #         #    # #      #####           # #  # #      #   #   ###### #      #      #      #####  
+@echo    #     #         #    # #      #               # #   ## #    #   #   #    # #      #      #      #   #  
+@echo    #     #         #####  ###### #               # #    #  ####    #   #    # ###### ###### ###### #    # 
+@echo			for Open Video Downloader Portable
+@echo											By MuhannadYT
+@echo off
+@echo Downloading yt-dlp...
+curl -LJO https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+@echo.
+@echo yt-dlp has been downloaded. Open Video Downloader should work now
+@echo If any issues occur report them here https://github.com/MuhannadYT/yt-dlp-installer-ovd
+timeout /t 10
